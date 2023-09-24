8324f22c31221162cc66aeeba8e15b4740cac202
DOS SANTOS FACAO Paul-Emmanuel <pedsf.fullstack@gmail.com>
2023-09-24 11:11:51 +0200

    add day 7

---
+++ 006/day-6-start.py
@@ -0,0 +1,14 @@
+# Somes functions are embeded in Python
+print("Hello")
+
+# A function can return value
+num_char = len("Hello")
+print(num_char)
+
+# You can create a function with def keyword
+def my_function():
+  print("Hello")
+  print("Bye")
+
+# call the finction with his name
+my_function()
