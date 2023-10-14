dc22acc819f3aa87812de66dfde48dfc53c3fa05
DOS SANTOS FACAO Paul-Emmanuel <pedsf.fullstack@gmail.com>
2023-10-14 10:49:55 +0200

    add Day 30 with new password manager

---
+++ 030/password-manager/password_generator.py
@@ -0,0 +1,19 @@
+import random
+DEFAULT_PASSWORD_LENGTH = 12
+
+lower_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+upper_letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+
+def password_create(length=DEFAULT_PASSWORD_LENGTH):
+    """Generate a password with the specified length return it"""
+    passwrd = ""
+    for _ in range(length):
+        char_list = [random.choice(lower_letters),
+                     random.choice(upper_letters),
+                     random.choice(numbers),
+                     random.choice(symbols)]
+        passwrd += random.choice(char_list)
+    return passwrd
